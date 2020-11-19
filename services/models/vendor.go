5c6f5fc016a8a717ac748db6de482b3860aa3802
saveliy <saveliy4kremen@gmail.com>
2020-11-19 10:16:54 +0200

    Update vendor.go

--- services/models/vendor.go
+++ services/models/vendor.go
@@ -3,21 +3,20 @@ package models
 import (
 	"database/sql"
 	v1 "gcms/api/v1"
-	"time"
 )
 
 type Vendor struct {
-	ID        uint32    `db:"id"`
-	CreatedAt time.Time `db:"created_at"`
+	ID        sql.NullInt32 `db:"id"`
+	CreatedAt sql.NullTime  `db:"created_at"`
 
-	Name    string         `db:"name"`
+	Name    sql.NullString `db:"name"`
 	Country sql.NullString `db:"country"`
 }
 
 func VendorToResponse(vendor Vendor) *v1.Vendor {
 	return &v1.Vendor{
-		Id:      vendor.ID,
-		Name:    vendor.Name,
+		Id:      uint32(vendor.ID.Int32),
+		Name:    vendor.Name.String,
 		Country: vendor.Country.String,
 	}
 }
